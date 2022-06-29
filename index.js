8419ab3ba54e074c324a902d25d5b208d36f94af
omarusmani <omarusmani3@hotmail.com>
2022-06-28 20:17:04 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+// require("dotenv").config()
+const app =express()
+const port= process.env.PORT || 4002
+
+app.get('/', (req, res) => {
+    res.send('Welcome to our server!')
+  })
+
+
+  app.listen(port, () => {
+    console.log(`Web server is listening on port ${port}!`);
+   });
