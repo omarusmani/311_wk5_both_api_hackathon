43bbc68ee4428e9877e3178b0a9600e7f254d11f
Strevino2 <strevino2@hotmail.com>
2022-06-30 20:59:01 -0500

    connected and working

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express")
 // require("dotenv").config()
-const app =express()
+const app = express()
 const port= process.env.PORT || 4002
+const usersRouter = require("./routes/index");
+
 
 app.use(express.json());
+app.use("/", usersRouter);
+
 app.get('/', (req, res) => {
     res.send('Welcome to our server!')
   })
-
 
   app.listen(port, () => {
     console.log(`Web server is listening on port ${port}!`);
